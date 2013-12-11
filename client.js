ba3b2fef77c024923a861eb1eb71e3f82b1881ba
Nathan Rajlich <nathan@tootallnate.net>
2013-12-11 14:27:26 -0800

    client: use "load-styles"

--- client.js
+++ client.js
@@ -1,31 +1,8 @@
 var css = "{css}";
-
-/**
- * Module exports.
- */
-
-module.exports = setup;
-
-/**
- * Injects the CSS into the <head> DOM node.
- */
-
-function setup (doc) {
-  // only run once per CSS file...
-  if (setup.called) return;
-  setup.called = true;
-
-  // default to the global `document` object
-  if (!doc) doc = document;
-
-  var head = doc.head || doc.getElementsByTagName('head')[0];
-  if (!head) throw new Error('could not find <head> DOM node');
-  var style = doc.createElement('style');
-  style.type = 'text/css';
-  if (style.styleSheet) {  // IE
-    style.styleSheet.cssText = css;
-  } else {                 // the world
-    style.appendChild(doc.createTextNode(css));
-  }
-  head.appendChild(style);
-}
+var called = false;
+var load = require('load-styles');
+module.exports = function loadStyles(doc) {
+  if (called) return;
+  called = true;
+  return load(css, doc);
+};
